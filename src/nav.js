37a2233b4abe8cf575b1e614d5891c6cf5f6aa51
etiry <7997357+etiry@users.noreply.github.com>
2023-07-15 14:01:55 -0400

    Add logo and logo font

--- src/nav.js
+++ src/nav.js
@@ -8,15 +8,34 @@ function render() {
   const navContainer = document.createElement('div');
   navContainer.setAttribute('id', 'nav-container');
 
+  const logoContainer = document.createElement('div');
+  logoContainer.setAttribute('id', 'logo-container');
+
+  const logo = document.createElement('img');
+  logo.setAttribute('src', 'img/logo.svg');
+  logo.setAttribute('id', 'logo');
+
+  const logoText = document.createElement('p');
+  logoText.innerText = 'Le Macaron Magnifique';
+
+  logoContainer.appendChild(logo);
+  logoContainer.appendChild(logoText);
+
+  const buttonContainer = document.createElement('div');
+  buttonContainer.setAttribute('id', 'button-container');
+
   const buttons = ['Home', 'Menu', 'Contact'];
 
   buttons.forEach(function (button) {
     const b = document.createElement('button');
     b.setAttribute('class', 'nav-button');
     b.innerText = button;
-    navContainer.appendChild(b);
+    buttonContainer.appendChild(b);
     b.addEventListener('click', linkNavBar);
   })
+
+  navContainer.appendChild(logoContainer);
+  navContainer.appendChild(buttonContainer);
 
   nav.appendChild(navContainer);
 }
