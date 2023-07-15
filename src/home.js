14c1db8eb48d498571aa30931c4534c1393ce423
etiry <7997357+etiry@users.noreply.github.com>
2023-07-15 13:25:41 -0400

    Fix page styling/formatting

--- src/home.js
+++ src/home.js
@@ -9,16 +9,20 @@ function render() {
   const mainHeadline = document.createElement('div');
   mainHeadline.setAttribute('id', 'main-headline');
 
-  const headline = document.createTextNode('Le Macaron Magnifique');
+  mainHeadline.innerHTML = '<p>Le Macaron Magnifique</p>'
+
+  // const headline = document.createTextNode('Le Macaron Magnifique');
 
   const mainText = document.createElement('div');
   mainText.setAttribute('id', 'main-text');
 
-  const text = document.createTextNode('Indulge in Parisian perfection at our bakery, where every bite is a journey of bliss. From classic raspberry to innovative lavender-infused macarons, experience a symphony of flavors and colors. Elevate your dessert game with our heavenly treats and discover the artistry of macarons at Le Macaron Magnifique.')
+  mainText.innerHTML = '<p>Indulge in Parisian perfection at our bakery, where every bite is a journey of bliss. From classic raspberry to innovative lavender-infused macarons, experience a symphony of flavors and colors. Elevate your dessert game with our heavenly treats and discover the artistry of macarons at Le Macaron Magnifique.</p>';
 
-  mainHeadline.appendChild(headline);
+  // const text = document.createTextNode('Indulge in Parisian perfection at our bakery, where every bite is a journey of bliss. From classic raspberry to innovative lavender-infused macarons, experience a symphony of flavors and colors. Elevate your dessert game with our heavenly treats and discover the artistry of macarons at Le Macaron Magnifique.')
 
-  mainText.appendChild(text);
+  // mainHeadline.appendChild(headline);
+
+  // mainText.appendChild(text);
 
   contentContainer.appendChild(mainHeadline);
   contentContainer.appendChild(mainText);
